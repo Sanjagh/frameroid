db2299f4984ccdd089361d61766d8e8d57d42fed
Mahmood <mahmood8664@gmail.com>
2016-09-19 09:52:19 +0430

    revise directories

--- frameroid/src/main/java/ir/saverin/frameroid/util/GeneralConfig.java
+++
@@ -1,9 +0,0 @@
-package ir.saverin.frameroid.util;
-
-/**
- * @author <a mailto:fahim.ayat@gmail.com>Fahim Ayat</a>
- */
-public class GeneralConfig {
-
-    public static boolean DEBUG = false;
-}
--- frameroid/src/main/java/ir/saverin/frameroid/widgets/keyboard/CodeKey.java
+++
@@ -1,8 +0,0 @@
-package ir.saverin.frameroid.widgets.keyboard;
-
-/**
- * @author <a mailto:fahim.ayat@gmail.com>Fahim Ayat</a>
- */
-public interface CodeKey extends Key {
-    int getCode();
-}
